afc5d9a65e17f1593d441d51b75cc18b367657ca
Muhammad Asim kharal Gogera <114603843+asim2110@users.noreply.github.com>
2024-09-03 11:30:35 -0700

    first commit

---
+++ lib/providers/to_do_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+import '../model/to_do_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TodoList with ChangeNotifier {
+  List<TodoModel> _todoList = [];
+
+  List<TodoModel> get todoList => _todoList;
+
+  void addToDO(TodoModel todoModel) {
+    _todoList = [todoModel, ..._todoList];
+    notifyListeners();
+  }
+
+  void removeTodo(TodoModel todoModel) {
+    _todoList.remove(todoModel);
+    notifyListeners();
+  }
+
+  void toDoTogel(int id, bool isCompleted) {
+    _todoList = [
+      for (final toDo in todoList)
+        if (toDo.id == id) toDo.copyWith(Completed: isCompleted) else toDo
+    ];
+  }
+}
+
+final todoListProvider = ChangeNotifierProvider((ref) => TodoList());
